3e611321265b446651fd88e9cbf4fb96b29211cd
knapdev <knapdev@gmail.com>
2021-01-12 03:18:10 -0600

    Added more API examples.

--- example.js
+++ example.js
@@ -4,8 +4,16 @@ function loop(){
 
     Keyboard._update();
 
+    if(Keyboard.getKey(Keyboard.KeyCode.SPACE)){
+        console.log("Space is held down!");
+    }
+
     if(Keyboard.getKeyDown(Keyboard.KeyCode.SPACE)){
-        console.log('Space pressed!');
+        console.log('Space was just pressed!');
+    }
+
+    if(Keyboard.getKeyDown(Keyboard.KeyCode.SPACE)){
+        console.log('Space was just released!');
     }
 
     requestAnimationFrame(loop);
